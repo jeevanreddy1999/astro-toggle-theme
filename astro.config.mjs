acfafac4f4f7d71d9331da1f5964bc1fabdefae1
Jeevan Karnati <jeevanreddy1999@gmail.com>
2023-06-25 20:12:55 +0530

    Initial commit.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import turbolinks from "@astrojs/turbolinks";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), turbolinks()]
+});
